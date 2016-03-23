72f8e055bd12518cf974c0807c755eac5681b874
wangjiachuan <93126721@qq.com>
2016-03-23 16:28:19 +0800

    init commit

---
+++ crawler.py
@@ -0,0 +1,23 @@
+#-*- coding: utf8 -*-
+import sys
+import time
+import traceback
+import os,time
+import random
+
+
+class Crawler(object):
+
+
+    def __init__(self):
+        pass 
+        
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+
+    myCrawler = Crawler()
+    myCrawler.main()
