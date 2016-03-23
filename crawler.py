e9432052c391067dbcd33bdd4da8356b858c302e
wangjiachuan <93126721@qq.com>
2016-03-23 17:30:26 +0800

    add login info

--- crawler.py
+++ crawler.py
@@ -19,7 +19,8 @@ class Crawler(object):
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookie))
         urllib2.install_opener(self.opener)
         self.urllogin = "http://mail.163.com/"
-        self.login = {"return":"index.php","username":"","password":""}
+        #self.login = {"return":"index.php","username":"W93126721","password":""}
+        self.login = {"return":"index.php","idPlaceholder":"W93126721@163.com","pwdPlaceholder":"198039"}
         pass 
         
 
